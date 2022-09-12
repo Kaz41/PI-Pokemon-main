0c6683d6a844034cccf289bfbd5fed4f5173f0f2
Martin Mendoza <martinmendoza-2000@hotmail.com>
2022-09-12 10:17:50 -0300

    s
    s

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
+    },
+    health: {
+      type: DataTypes.DOUBLE
+    },
+    attack: {
+      type: DataTypes.DOUBLE
+    },
+    defense: {
+      type: DataTypes.DOUBLE
+    },
+    speed: {
+      type: DataTypes.DOUBLE
+    },
+    height: {
+      type: DataTypes.DOUBLE
+    },
+    weight: {
+      type: DataTypes.DOUBLE
     },
   });
 };
