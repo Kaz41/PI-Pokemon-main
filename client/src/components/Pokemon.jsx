b0598b80b04e9a151a5cb5d3df2f5a49a745dda9
Martin Mendoza <martinmendoza-2000@hotmail.com>
2022-09-25 03:58:03 -0300

    j
    j

--- client/src/components/Pokemon.jsx
+++ client/src/components/Pokemon.jsx
@@ -1,10 +1,7 @@
-
 import React from "react";
 
 export default async function Pokemon() {
-    fetch("http://localhost:3001/pokemons").then(res => res.json()).then(json => {console.log(json)})
-
     return (
-        <h3>{axios.get("http://localhost:3001/pokemons/7")}</h3>
+        <h3>Pokemon</h3>
     );
 }
